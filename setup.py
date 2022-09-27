acac4c54f5916ae54efe7abf819e0dc080457de7
Etienne Doumazane <etienne.doumazane@gmail.com>
2022-09-27 16:22:08 +0200

    Added example notebook

---
+++ setup.py
@@ -0,0 +1,6 @@
+# setup.py
+from setuptools import setup
+
+setup(name='tools_edmz',
+      description="tools for icm data exploration",
+      packages=["tools_edmz"]) # You can have several packages, try it
